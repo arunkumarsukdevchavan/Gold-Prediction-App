afc030a9bda4650a145c26c30e91ca462d6bb8de
arunkumarsukdevchavan <118343978+arunkumarsukdevchavan@users.noreply.github.com>
2024-09-04 10:23:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -54,9 +54,6 @@ def login_page():
         """,
         unsafe_allow_html=True
     )
-    .stApp h1 {
-  color: #f5f5f5;  /* Lighter shade of white */
-    }
     st.title("Login Page")
 
     st.session_state.username = st.text_input("Username")
