dc5fc4fd360a7356aed02af070eb0ef6c1ac9653
arunkumarsukdevchavan <118343978+arunkumarsukdevchavan@users.noreply.github.com>
2024-09-04 10:47:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -59,10 +59,14 @@ def login_page():
         unsafe_allow_html=True
     )
 
-    st.title("Login Page")
+    st.markdown("<h1 style='color: white;'>Login Page</h1>", unsafe_allow_html=True)
 
-    st.session_state.username = st.text_input("Username")
-    st.session_state.password = st.text_input("Password", type="password")
+    st.markdown("<p style='color: white;'>Username</p>", unsafe_allow_html=True)
+    st.session_state.username = st.text_input("")
+
+    st.markdown("<p style='color: white;'>Password</p>", unsafe_allow_html=True)
+    st.session_state.password = st.text_input("", type="password")
+
 
     if st.button("Login"):
         login()
