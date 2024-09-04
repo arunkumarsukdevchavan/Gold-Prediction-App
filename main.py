93adeb50cf58047365e9e9ea2a60183600192cf8
arunkumarsukdevchavan <118343978+arunkumarsukdevchavan@users.noreply.github.com>
2024-09-04 11:13:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -151,7 +151,7 @@ def process_and_train(gold_prices, economic_data):
     # Predict future gold price based on new economic data
     future_economic_data = np.array([[future_Inflation_Rate, future_Unemployment_Rate, future_GDP, future_Applied]])
     future_gold_price = model.predict(future_economic_data)
-    st.write(f'Predicted Gold Price: {future_gold_price[0]}')
+    st.header(f'Predicted Gold Price: {future_gold_price[0]}')
 
 # Main application page
 def app_page():
