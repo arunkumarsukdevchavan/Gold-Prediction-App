e05de8a98a9186cd1ca67103024889d258abd951
arunkumarsukdevchavan <118343978+arunkumarsukdevchavan@users.noreply.github.com>
2024-09-24 13:24:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.neighbors import KNeighborsRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 import matplotlib.pyplot as plt
